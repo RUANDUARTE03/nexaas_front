b57b25f6202df69fc04fddf09f1fc489c0ee509a
Lucas Britto da Silva <lucasbritto123@hotmail.com>
2022-03-28 10:12:10 -0300

    wip

--- playwright/providers/providers.spec.ts
+++ playwright/providers/providers.spec.ts
@@ -2,10 +2,45 @@ import { test, expect } from '@playwright/test';
 
 const REACT_URL = process.env.REACT_URL ?? '';
 const PROVIDER_HOME = `${REACT_URL}/providers`;
+const PROVIDER_CREATE = `${PROVIDER_HOME}/create`;
+const HOME_URL = process.env.HOME_URL ?? '';
 
-test.describe('New Todo', () => {
-  test('Menu click should redirect', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+const completeProvider = {
+  city: 'Niterói',
+  cityCode: '3303302',
+  country: 'Brazil',
+  detail: 'teste',
+  document: '29979036000140',
+  externalId: '1234',
+  name: 'teste 123',
+  neighborhood: 'Largo da Batalha',
+  number: '21',
+  providerType: 'Fabricante',
+  state: 'RJ',
+  stateInscription: '12341234',
+  stateInscriptionType: '1',
+  street: 'Rua Miguel Pereira Sarmento',
+  tradingName: 'teste 123',
+  zipcode: '24310425',
+};
+
+const onlyRequiredProvider = {
+  name: 'required provider',
+  document: '34028316000294',
+  providerType: 'Fabricante',
+};
+
+test.describe.serial('New Todo', () => {
+  let page;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+  test('Menu click should redirect', async () => {
+    await page.goto(HOME_URL);
 
     await page
       .locator('button:has-text("Cadastros")')
@@ -13,39 +48,114 @@ test.describe('New Todo', () => {
 
     await Promise.all([
       page.waitForNavigation({
-        url: 'http://localhost:3001/providers',
+        url: PROVIDER_HOME,
       }),
       page.locator('a:has-text("Fornecedores")').click(),
     ]);
 
-    await expect(page).toHaveURL(
-      'http://localhost:3001/providers'
-    );
+    await expect(page).toHaveURL(PROVIDER_HOME);
   });
 
-  test('Clique botão novo fornecedor deve entrar no cadastro', async ({
-    page,
-  }) => {
+  test('Clique botão novo fornecedor deve entrar no cadastro', async () => {
     await Promise.all([
       page.waitForNavigation({
-        url: 'http://localhost:3001/providers/create',
+        url: PROVIDER_CREATE,
       }),
       page
         .locator('[data-testid="btn-create-provider"]')
         .click(),
     ]);
 
-    await expect(page).toHaveURL(
-      'http://localhost:3001/providers/create'
-    );
+    await expect(page).toHaveURL(PROVIDER_CREATE);
+  });
+
+  test('Criar novo fornecedor', async () => {
+    await page
+      .locator('[data-cy=identifier] > input')
+      .fill(completeProvider.document);
+
+    await page
+      .locator('[data-cy=companyName] > input')
+      .fill(completeProvider.name);
+
+    await page
+      .locator('[data-cy=fantasyName] > input')
+      .fill(completeProvider.tradingName);
+
+    await page
+      .locator('[data-cy=typeProvider] > select')
+      .selectOption(completeProvider.providerType);
+
+    await page
+      .locator('[data-cy=indicatorSign] > select')
+      .selectOption(completeProvider.stateInscriptionType);
+
+    await page
+      .locator('[data-cy=identifierExternal] > input')
+      .fill(completeProvider.externalId);
+
+    await page
+      .locator('[data-cy=stateInscription] > input')
+      .fill(completeProvider.stateInscription);
+
+    await page
+      .locator('[data-cy=formattedZipCode] > input')
+      .fill(completeProvider.zipcode);
+
+    await page
+      .locator('[data-cy=stateName] > select')
+      .selectOption(completeProvider.state);
+
+    await Promise.all([
+      page.waitForNavigation({
+        url: PROVIDER_HOME,
+      }),
+      page
+        .locator('[data-testid="btn-createOrEditProvider"]')
+        .click(),
+    ]);
+
+    await expect(page).toHaveURL(PROVIDER_HOME);
+  });
+
+  test('Criar novo fornecedor somente com dados obrigatórios', async () => {
+    await Promise.all([
+      page.waitForNavigation({
+        url: PROVIDER_CREATE,
+      }),
+      page
+        .locator('[data-testid="btn-create-provider"]')
+        .click(),
+    ]);
+
+    await page
+      .locator('[data-cy=identifier] > input')
+      .fill(onlyRequiredProvider.document);
+
+    await page
+      .locator('[data-cy=companyName] > input')
+      .fill(onlyRequiredProvider.name);
+
+    await page
+      .locator('[data-cy=typeProvider] > select')
+      .selectOption(onlyRequiredProvider.providerType);
+
+    await Promise.all([
+      page.waitForNavigation({
+        url: PROVIDER_HOME,
+      }),
+      page
+        .locator('[data-testid="btn-createOrEditProvider"]')
+        .click(),
+    ]);
+
+    await expect(page).toHaveURL(PROVIDER_HOME);
   });
 });
 
-/* iit('Criar novo fornecedor', () => {
-});
+/* 
 
-it('Criar novo fornecedor com dados obrigatórios', () => {
-});
+
 
 it('Listagem de fornecedores com dados completos', () => {
 });
